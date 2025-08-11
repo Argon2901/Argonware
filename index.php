8cb4f0b693a346229a786a1285efeb4848372427
Collin <collint@gitlab.aerospace.mtu.edu>
2025-08-10 23:30:54 -0400

    Fixed missing semicolon

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include "components.php";
         <meta name="author" content="Collin Thompson">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <style><?php include "style.css" ?></style>
+        <style><?php include "style.css"; ?></style>
 
     </head>
 
