9774d8da18f0b5f2751e3888c48c5a9379562c9f
Collin <collint@gitlab.aerospace.mtu.edu>
2025-08-11 18:40:38 -0400

    Brought all pages into compliance with php usage.

--- aboutme.php
+++ aboutme.php
@@ -5,16 +5,8 @@ include "components.php";
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>I'll Think of a Title Name Later</title>
-        <meta charset="UTF-8">
-        <meta name="author" content="Collin Thompson">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <style><?php include "style.css"; ?></style>
-
-    </head>
-
+    <?php echo http_head();?>
+    
     <body>
         <?php echo header_template();?>
 
--- components.php
+++ components.php
@@ -1,6 +1,20 @@
 <?php
 declare (strict_types=1);
 
+function http_head(): string{
+    return <<<EOD
+    <head>
+        <title>I'll Think of a Title Name Later</title>
+        <meta charset="UTF-8">
+        <meta name="author" content="Collin Thompson">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <style><?php include "style.css"; ?></style>
+
+    </head>
+EOD;
+}
+
 function header_template(): string{ 
     return <<<EOD
     <div id="menubar">
