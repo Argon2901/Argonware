e95f83131f8eb09592768f6b2b908705480a2652
Collin <collint@gitlab.aerospace.mtu.edu>
2025-08-10 23:28:06 -0400

    Added header echo to file

--- components.php
+++ components.php
@@ -1,4 +1,6 @@
 <?php
+declare (strict_types=1);
+
 $headertemplate = `
     <div id="menubar">
         <a href="index.php">
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+include "components.php";
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,11 +12,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <style><?php include "style.css" ?></style>
-        <script src="index.js" type="text/javascript" defer></script>
 
     </head>
 
     <body>
+        <?php echo $headertemplate;?>
         The personal website of Collin Thompson. 
     </body>
 </html>
