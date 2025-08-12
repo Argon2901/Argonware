3d5871b2ea84c49006069741e97661614d024c52
Collin <collint@gitlab.aerospace.mtu.edu>
2025-08-12 13:06:41 -0400

    Restructured api to be separate files per call

--- API/send_message.php
+++ API/send_message.php
@@ -1,7 +1,7 @@
 <?php
 declare (strict_types=1);
 
-$ini = parse_ini_file(".cfg");
+$ini = parse_ini_file(".ini");
 /**
  * Recieves information from a contact post and emails it to my email.
  */
@@ -10,5 +10,5 @@ function send_message(string $message, string $email, string $phone): int{
     $headers[] = "Content-type: text/html; charset=UTC-8";
     $headers[] = "TO: " . $ini["email"]["name"] . " <" . $ini["email"]["email"] . ">";
     $headers[] = "messenger@" . $ini["website"]["url"];
-    mail($ini["email"]["email"], "Message from " . $email, $message);   
+    mail($ini["email"]["email"], "Message from " . $email, $message, $headers);   
 }
