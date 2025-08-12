e867cab2e0e766f09ba9ae44b3f1740ff7101d79
Collin <collint@gitlab.aerospace.mtu.edu>
2025-08-12 18:43:51 -0400

    Changed website title to match new branding. Improved CSS layout.

--- components.php
+++ components.php
@@ -4,7 +4,7 @@ declare (strict_types=1);
 function http_head(): string{
     return <<<EOD
     <head>
-        <title>I'll Think of a Title Name Later</title>
+        <title>Argonware</title>
         <meta charset="UTF-8">
         <meta name="author" content="Collin Thompson">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
